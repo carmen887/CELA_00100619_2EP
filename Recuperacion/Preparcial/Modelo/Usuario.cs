f45adb4e7318ffcfff39e1e11318b1500d68fdf3
carmen887 <54786234+carmen887@users.noreply.github.com>
2020-05-31 21:07:29 -0600

    Add: Recuperacion (Pre Parcial)

---
+++ Recuperacion/Preparcial/Modelo/Usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace Preparcial.Modelo
+{
+    public class Usuario
+    {
+        public string IdUsuario { get; set; }
+
+        public string NombreUsuario { get; set; }
+        
+        //Correccion: cambiar "contrasena" por "contrasenia"
+        public string Contrasenia { get; set; }
+
+        public bool Admin { get; set; }
+
+        public Usuario(string idUsuario, string nombreUsuario, string contrasenia, bool admin)
+        {
+            IdUsuario = idUsuario;
+            NombreUsuario = nombreUsuario;
+            Contrasenia = contrasenia;
+            Admin = admin;
+        }
+    }
+}
